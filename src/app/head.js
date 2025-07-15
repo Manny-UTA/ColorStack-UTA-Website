de521be7c45018a2f569ca1fd9d3b0f1225009ff
Manny-UTA <Mja5882@mavs.uta.edu>
2025-07-15 22:42:21 +0000

    Add head.js for viewport and meta tags

---
+++ src/app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>ColorStack UTA</title>
+      <meta name="description" content="ColorStack UTA Website" />
+    </>
+  )
+}

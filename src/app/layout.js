c695920df3b7882b6b2a9bd0489ff352e1731cc2
Manny-UTA <Mja5882@mavs.uta.edu>
2025-07-31 07:18:02 +0000

    chore: add Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,14 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
+import { Analytics } from '@vercel/analytics/react'
+
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'ColorStack UTA',
+  description: 'ColorStack UTA Website',
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -13,6 +20,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
+        <Analytics /> 
       </body>
     </html>
   )
